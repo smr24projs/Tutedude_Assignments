5d3019bdd218372cb84ba7b597bd70bc55cbe39d
smr24projs <sarthakmudholkar@gmail.com>
2025-11-14 10:19:22 +0530

    Added Assignment 1 Task 1

---
+++ Assignment1_Task1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+add = num1 + num2
+sub = num1 - num2
+multi = num1 * num2
+div = num1/num2
+
+print("Addition:",add)
+print("Subtraction:",sub)
+print("Multiplication:",multi)
+print("Division:",round(div,1))
